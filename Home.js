c5506e05d959e646e1b8a119a78c2a6791fdb138
prosoul <41618544+sourabh58@users.noreply.github.com>
2022-12-19 17:48:04 +0530

    Create a home component

---
+++ Home.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Home() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/appointments');
+      setAppointments(result.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Upcoming Appointments</h1>
+      {appointments.map(appointment => (
+        <div key={appointment.id}>
+          <h2>{appointment.service}</h2>
+          <p>Date: {appointment.date}</p>
+          <p>Time: {appointment.time}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
